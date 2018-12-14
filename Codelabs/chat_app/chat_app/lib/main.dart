52999010952e33871709a09a80ce50f6a6b97ea2
Derek Hu <derekh@datacom.co.nz>
2018-12-14 14:23:01 +1300

    added text box

--- Codelabs/chat_app/chat_app/lib/main.dart
+++ Codelabs/chat_app/chat_app/lib/main.dart
@@ -7,6 +7,7 @@ The widget consists of a Scaffold widget that has a simple AppBar as its child w
  */
 import 'package:flutter/material.dart';
 
+const String _name = "DKH";
 void main() {
   runApp(new FriendlychatApp());
 }
@@ -46,6 +47,10 @@ class ChatScreen extends StatefulWidget {                     //modified
 //To manage interactions with the text field, it's helpful to use a TextEditingController object.
 //You'll use it for reading the contents of the input field,
 //and for clearing the field after the text message is sent.
+//A BuildContext object is a handle to the location of a widget in your app's widget tree.
+//Each widget has its own BuildContext, which becomes the parent of the widget returned by the StatelessWidget.build or State.build function.
+//This means the _buildTextComposer() method can access the BuildContext object from its encapsulating State object;
+//you don't need to pass the context to the method explicitly.
 class ChatScreenState extends State<ChatScreen> {
   final TextEditingController _textController = new TextEditingController();
   @override                                                        //new
@@ -60,15 +65,35 @@ class ChatScreenState extends State<ChatScreen> {
   // Add the following code in the ChatScreenState class definition.
   //The following code snippet shows how you can define a private method called _buildTextComposer()
   //that returns a Container widget with a configured TextField widget.
+  //Please bear in mind Container and Padding can only contain on child widget.
   Widget _buildTextComposer() {
     return new Container(
       margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: new TextField(
-        controller: _textController,
-        onSubmitted: _handleSubmitted,
-        decoration: new InputDecoration.collapsed(
-            hintText: "Send a message"),
-      ),
+      //Since we want to display the button adjacent to the input field, we'll use a Row widget as the parent
+      child: new Row(                                            //new
+        children: <Widget>[
+          //Then wrap the TextField widget in a Flexible widget.
+          //This tells the Row to automatically size the text field to use the remaining space that isn't used by the button.
+          new Flexible(                                          //new
+            child: new TextField(
+              controller: _textController,
+              onSubmitted: _handleSubmitted,
+              decoration: new InputDecoration.collapsed(
+                  hintText: "Send a message"),
+            ),
+          ),
+
+          //Put your IconButton widget inside another Container parent widget;
+          //this lets you customize the margin spacing of the button so that it visually fits better next to your input field.
+          new Container(                                                 //new
+            margin: new EdgeInsets.symmetric(horizontal: 4.0),           //new
+            child: new IconButton(                                       //new
+                icon: new Icon(Icons.send),                                //new
+                color: Colors.blue,
+                onPressed: () => _handleSubmitted(_textController.text)),  //new
+          ),
+        ],                                                        //new
+      ),                                                          //new
     );
   }
 
@@ -76,6 +101,36 @@ class ChatScreenState extends State<ChatScreen> {
   //Tip: Prefixing an identifier with an _ (underscore) makes it private to its class
   void _handleSubmitted(String text) {
     _textController.clear();
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: new CircleAvatar(child: new Text(_name[0])),
+          ),
+          new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Text(_name, style: Theme.of(context).textTheme.subhead),
+              new Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: new Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
 
